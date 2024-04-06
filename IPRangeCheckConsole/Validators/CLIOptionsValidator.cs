58287e5a171484a3852a121daa2bc07a39ecdf2f
dev-Macintosh <ivan.200512340@gmail.com>
2024-04-06 23:21:04 +0300

    Add FluentValidation and Delete Custom attribute

---
+++ IPRangeCheckConsole/Validators/CLIOptionsValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using IPRangeCheckConsole.Misc;
+
+namespace IPRangeCheckConsole.Validators
+{
+    public class CLIOptionsValidator : AbstractValidator<CLIOptions>
+    {
+        public CLIOptionsValidator()
+        {
+            RuleFor(opts => opts.FileLog).NotNull().ValidateFilePathAddress();
+
+            RuleFor(opts => opts.FileOutput).NotNull().ValidateFilePathAddress();
+
+            RuleFor(opts => opts.AddressStart).NotNull().ValidateIPAddress();
+
+            RuleFor(opts => opts.AddressMask).NotNull().ValidateIPAddress();
+
+            RuleFor(opts => opts.TimeStart).NotNull().ValidateDateTime();
+
+            RuleFor(opts => opts.TimeEnd).NotNull().ValidateDateTime();
+        }
+    }
+}
---
+++ IPRangeCheckConsole/Validators/FluentValidationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System.Globalization;
+using System.Net;
+
+namespace IPRangeCheckConsole.Validators
+{
+    public static class FluentValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, string> ValidateIPAddress<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                       .Must(val => IPAddress.TryParse(val, out _)).WithMessage("У свойства {PropertyName} неправильно задан IP-Address.");
+        }
+
+        public static IRuleBuilderOptions<T, string> ValidateFilePathAddress<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                       .Must(val =>
+                       {
+                           FileInfo fileInfo = new FileInfo(val);
+
+                           if (!fileInfo.Exists)
+                               return false;
+                           return true;
+                       }).WithMessage("У свойства {PropertyName} неправильно задан путь к файлу.");
+
+        }
+
+        public static IRuleBuilderOptions<T, DateTime?> ValidateDateTime<T>(this IRuleBuilder<T, DateTime?> ruleBuilder)
+        {
+            return ruleBuilder
+                       .Must(val =>
+                       {
+                           return DateTime.TryParseExact(val.ToString(), "d.M.yyyy", null, DateTimeStyles.None, out DateTime dateTime);
+                       }).WithMessage("У свойства {PropertyName} неправильно задана дата или её формат.");
+
+        }
+    }
+}
